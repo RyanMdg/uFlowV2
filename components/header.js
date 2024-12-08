10a3a6a3874fc4726edd2961ae4127db92f2ccb8
RyanMdg <ryandeguia.dev@gmail.com>
2024-12-09 02:36:39 +0800

    back btn in map

--- components/header.js
+++ components/header.js
@@ -1,24 +1,42 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
-const header = () => {
+const Header = () => {
+  const navigation = useNavigation();
+
+  // Handle back button action
+  const handleBackPress = () => {
+    navigation.goBack();
+  };
+
   return (
     <View style={styles.container}>
+      {/* Back Button */}
+      <TouchableOpacity onPress={handleBackPress} style={styles.backButton}>
+        <AntDesign name="arrowleft" size={24} color="black" />
+      </TouchableOpacity>
+
+      {/* User Image */}
       <Image
         source={require("../assets/images/user2.png")}
-        style={{ width: 60, height: 60, borderRadius: 99, borderWidth: 2 }}
+        style={styles.userImage}
       />
+
+      {/* Logo */}
       <Image
         source={require("../assets/images/logoblack.png")}
-        style={{ width: 150, height: 45, objectFit: "contain" }}
+        style={styles.logo}
       />
+
+      {/* Filter Icon */}
       <AntDesign name="filter" size={24} color="black" />
     </View>
   );
 };
 
-export default header;
+export default Header;
 
 const styles = StyleSheet.create({
   container: {
@@ -27,5 +45,19 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
     paddingHorizontal: 5,
+  },
+  backButton: {
+    padding: 5,
+  },
+  userImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 99,
+    borderWidth: 2,
+  },
+  logo: {
+    width: 150,
+    height: 45,
+    resizeMode: "contain",
   },
 });
